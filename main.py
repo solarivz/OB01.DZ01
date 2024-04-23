72b7c803e0d7dd2f042f40772222584bb093a0c5
Ivan Zyulin <solar.ivz@gmail.com>
2024-04-24 00:48:53 +1200

    add new code

--- main.py
+++ main.py
@@ -4,29 +4,49 @@
 # (выполнено/не выполнено). Реализуй функцию для добавления задач, отметки
 # выполненных задач и вывода списка текущих (не выполненных) задач.
 
-
 class Task():
-    def __init__(self, task_description, task_deadline, task_status):
-        # Инициализация атрибутов обьекта класса начальными значениями
-        self.task_description = task_description              # Описание задачи
-        self.task_deadline = task_deadline                 # Время выполнения задачи
-        self.task_status = task_status                # Статус задачи
+    def __init__(self, description, deadline):
+        self.description = description          # описание задачи
+        self.deadline = deadline                # срок выполнения задачи
+        self.completed = False                  # изначально задача не выполнена
 
-def add_task():
-    task_list.append([self.task_description, self.task_deadline, self.task_status])
 
-def list_tasks(self):
+# Функция для добавления новой задачи - добавляется задача типа обьект вновь
+# созданный с атрибутами переданными функции
+
+def add_task(tasks, description, deadline):
+    tasks.append(Task(description, deadline))
+    print(f"Задача {tasks} добавлена в список\n")
+
+
+# Функция для отметки задачи как выполненной
+def mark_task_as_completed(task):
+    task.completed = True
+    print(f"Отмечаем задачу {task} как выполненную")
+    print()
+
+# Функция для вывода списка задач
+def list_tasks(tasks, completed=False):
     print("Список задач:")
-    for i in range(len(self.task_list)):
-            self.task_description, self.task_deadline, self.task_status = self.task_list[i]
-            self.task_stat = "Выполнено" if self.task_status else "Не выполнено"
-            print(f"{i + 1}. Описание: {self.task_description}, Срок выполнения: {self.task_deadline}, Статус: {self.task_stat}")
+    for i, task in enumerate(tasks, start=1):
+        if completed == task.completed:
+            status = "Выполнено" if task.completed else "Не выполнено"
+            print(f"{i}. Описание: {task.description}, Срок выполнения: {task.deadline}, Статус: {status}")
+    print()
 
+# Пример использования
 
-task_list = []
-task_manager = Task()
-task_manager.task_description, task_manager.task_deadline, task_manager.task_status = ('Подготовить презентацию', "15.05.2024", False)
-task_manager.add_task()
+tasks = []  # Список задач
+
+# Добавляем задачи через вызов функции, передаем аргументы ссылку на обьект типа список, и две строковые переменные
+add_task(tasks, "Сделать уроки", "30.04.2024")
+add_task(tasks, "Подготовить презентацию", "15.05.2024")
 
 # Выводим список задач
-task_manager.list_tasks()
+list_tasks(tasks)
+
+# Отмечаем первую задачу как выполненную
+mark_task_as_completed(tasks[0])
+
+# Выводим список задач
+list_tasks(tasks)
