b514aefb113727eb3ac9c3ead4d1448379437246
Ivan Zyulin <solar.ivz@gmail.com>
2024-04-24 00:58:09 +1200

    add new code

--- main.py
+++ main.py
@@ -4,49 +4,48 @@
 # (выполнено/не выполнено). Реализуй функцию для добавления задач, отметки
 # выполненных задач и вывода списка текущих (не выполненных) задач.
 
-class Task():
+class Task:
     def __init__(self, description, deadline):
-        self.description = description          # описание задачи
-        self.deadline = deadline                # срок выполнения задачи
-        self.completed = False                  # изначально задача не выполнена
+        self.description = description
+        self.deadline = deadline
+        self.completed = False
 
-
-# Функция для добавления новой задачи - добавляется задача типа обьект вновь
-# созданный с атрибутами переданными функции
-
+# Функция для добавления новой задачи
 def add_task(tasks, description, deadline):
-    tasks.append(Task(description, deadline))
-    print(f"Задача {tasks} добавлена в список\n")
-
-
-# Функция для отметки задачи как выполненной
-def mark_task_as_completed(task):
-    task.completed = True
-    print(f"Отмечаем задачу {task} как выполненную")
-    print()
+    task = Task(description, deadline)
+    tasks.append(task)
+    print(f"Задача '{task.description}' срок выполнения '{task.deadline}' добавлена в список.\n")
 
 # Функция для вывода списка задач
 def list_tasks(tasks, completed=False):
     print("Список задач:")
-    for i, task in enumerate(tasks, start=1):
+    i = 1
+    for task in tasks:
         if completed == task.completed:
             status = "Выполнено" if task.completed else "Не выполнено"
-            print(f"{i}. Описание: {task.description}, Срок выполнения: {task.deadline}, Статус: {status}")
+            print(f"{i}. Описание: '{task.description}', Срок выполнения: '{task.deadline}', Статус: {status}")
+            i += 1
     print()
 
-# Пример использования
+# Функция для отметки задачи как выполненной
+def mark_task_as_completed(task):
+    task.completed = True
+    print(f"Задача '{task.description}', Срок выполнения: '{task.deadline}', Статус: Выполнена\n")
 
+
+# Пример использования
 tasks = []  # Список задач
 
-# Добавляем задачи через вызов функции, передаем аргументы ссылку на обьект типа список, и две строковые переменные
-add_task(tasks, "Сделать уроки", "30.04.2024")
-add_task(tasks, "Подготовить презентацию", "15.05.2024")
+# Добавляем задачи
+add_task(tasks, "Выполнить задание 1 ", "30.04.2024")
+add_task(tasks, "Подготовиться к экзамену", "01.05.2024")
 
 # Выводим список задач
 list_tasks(tasks)
 
 # Отмечаем первую задачу как выполненную
 mark_task_as_completed(tasks[0])
+
 
 # Выводим список задач
 list_tasks(tasks)
